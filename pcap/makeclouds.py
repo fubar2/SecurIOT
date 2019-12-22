9592e2622f70bfede079f29fc0bd79fcd79bd614
fubar2 <ross.lazarus@gmail.com>
2019-12-23 09:10:04 +1100

    Can draw network graph images now but not much use - too complex to show all IP and same information as wordcloud for each individual IP address network connections

--- pcap/makeclouds.py
+++ pcap/makeclouds.py
@@ -8,134 +8,168 @@ import socket
 import os
 from random import randint
 
-infname = '/home/ross/rossgit/pcap/eg2.pcap'
+# infname = '/home/ross/rossgit/pcap/eg2.pcap'
 infname = '/home/ross/rossgit/pcap/example.pcap'
 
 pnames = ['IP','TCP','ARP','UDP','ICMP']
 pobj = [IP,TCP,ARP,UDP,ICMP]
 
+doGraphs = False # these are same as wordclouds for each ip and too big for all ip/port to make any sense AFAIK
+
+if doGraphs: 
+	# network graph construction and plotting is relatively easy but 
+	# not useful for single sources and too messy if all IP.
+	import networkx as nx
+	gIP = nx.Graph()
+	gPORT = nx.Graph()
+
 def random_color_func(word=None, font_size=None, position=None,  orientation=None, font_path=None, random_state=None):
 	"""https://stackoverflow.com/questions/43043263/word-cloud-in-python-with-customised-colour"""
-    h = int(360.0 * 21.0 / 255.0) # orange base
-    s = int(100.0 * 255.0 / 255.0)
-    l = int(100.0 * float(randint(60, 120)) / 255.0)
+	h = int(360.0 * 21.0 / 255.0) # orange base
+	s = int(100.0 * 255.0 / 255.0)
+	l = int(100.0 * float(randint(60, 120)) / 255.0)
 
-    return "hsl({}, {}%, {}%)".format(h, s, l)
+	return "hsl({}, {}%, {}%)".format(h, s, l)
 
 def getsrcdest(pkt,proto):
-    """need from every packet of interest"""
-    saucen = None
-    destn = None
-    dport = None
-    sport = None
-    if IP in pkt:
-        saucen = pkt[IP].src
-        destn = pkt[IP].dst
-    if (TCP in pkt):
-        sport = pkt[TCP].sport
-        dport = pkt[TCP].dport
-    elif UDP in pkt:
-        sport = pkt[UDP].sport
-        dport = pkt[UDP].dport
-        
-    return (saucen,destn,str(sport),str(dport))   
+	"""need from every packet of interest"""
+	saucen = None
+	destn = None
+	dport = None
+	sport = None
+	if IP in pkt:
+		saucen = pkt[IP].src
+		destn = pkt[IP].dst
+	if (TCP in pkt):
+		sport = pkt[TCP].sport
+		dport = pkt[TCP].dport
+	elif UDP in pkt:
+		sport = pkt[UDP].sport
+		dport = pkt[UDP].dport
+		
+	return (saucen,destn,str(sport),str(dport))   
 
 def lookup(sauce,sourcen,deens):
-    """deens caches all slow! fqdn reverse dns lookups from ip"""
-    kname = deens.get(sourcen)
-    if kname == None:
-        kname = socket.getfqdn(sourcen) # PIA dns is slow!!
-        deens[sourcen] = kname
-    newsaucen = kname
-    sk = sauce.keys()
-    newsauce = {}
-    for k in sk:
-        kname = deens.get(k,None)
-        if kname == None:
-            kname = socket.getfqdn(k)
-            deens[k] = kname
-        newsauce[kname] = sauce[k]
-    return (newsauce,newsaucen)
+	"""deens caches all slow! fqdn reverse dns lookups from ip"""
+	kname = deens.get(sourcen)
+	if kname == None:
+		kname = socket.getfqdn(sourcen) # PIA dns is slow!!
+		deens[sourcen] = kname
+	newsaucen = kname
+	sk = sauce.keys()
+	newsauce = {}
+	for k in sk:
+		kname = deens.get(k,None)
+		if kname == None:
+			kname = socket.getfqdn(k)
+			deens[k] = kname
+		newsauce[kname] = sauce[k]
+	return (newsauce,newsaucen)
 
 def readPcap(infile,seenIP,seenPORT):
-    """single pass version """
-    for i,proto in enumerate(pobj):
-            pn = pnames[i]
-            seenIP[pn] = {}
-    for pkt in PcapReader(infile):
-        for i,proto in enumerate(pobj):
-            pn = pnames[i]
-            if proto in pkt:
-                nsauce,ndest,sport,dport = getsrcdest(pkt,proto)
-                bingo = False
-                ipport = '%s_%s' % (nsauce,sport)
-                if seenPORT.get(ipport,None) == None:
-                    c = Counter()
-                    seenPORT[ipport] = c
-                    seenPORT[ipport][dport] = 1
-                    bingo = True
-                else:
-                    seenPORT[ipport][dport] += 1
-                    bingo = True
-                if seenIP[pn].get(nsauce,None):
-                    seenIP[pn][nsauce][ndest] += 1
-                    bingo = True
-                else:
-                    c = Counter()
-                    seenIP[pn][nsauce] = c
-                    seenIP[pn][nsauce][ndest] = 1
-                    bingo = True
-                if bingo:
-                    continue
-    return(seenIP,seenPORT)
+	"""single pass version """
+	allIP = set()
+	allPORT = set()
+	for i,proto in enumerate(pobj):
+			pn = pnames[i]
+			seenIP[pn] = {}
+	for pkt in PcapReader(infile):
+		for i,proto in enumerate(pobj):
+			pn = pnames[i]
+			if proto in pkt:
+				nsauce,ndest,sport,dport = getsrcdest(pkt,proto)
+				bingo = False
+				ipport = '%s_%s' % (nsauce,sport)
+				if seenPORT.get(ipport,None) == None:
+					c = Counter()
+					seenPORT[ipport] = c
+					seenPORT[ipport][dport] = 1
+					allPORT.add(ipport)
+					bingo = True
+				else:
+					seenPORT[ipport][dport] += 1
+					bingo = True
+				if seenIP[pn].get(nsauce,None):
+					seenIP[pn][nsauce][ndest] += 1
+					bingo = True
+				else:
+					c = Counter()
+					seenIP[pn][nsauce] = c
+					seenIP[pn][nsauce][ndest] = 1
+					allIP.add(nsauce)
+					allIP.add(ndest)
+					bingo = True
+				if bingo:
+					continue
+	return(seenIP,seenPORT,allIP,allPORT)
 
-          
+		  
 def processPcap(seenIP,seenPORT,deens):
-    for i,proto in enumerate(pobj):
-        pn = pnames[i]
-        for nsauce in seenIP[pn].keys():
-            kl = len(seenIP[pn][nsauce].keys())
-            print('###for',nsauce,'got',kl)
-            if kl > 1:
-                sf,newsaucen = lookup(seenIP[pn][nsauce],nsauce,deens) # expensive operation so moved here
-                # print(newsaucen,':',sf)
-                outfn = '%s_%s_wordcloud_%s.png' % (newsaucen,pn,os.path.basename(infname))
-                wc = WordCloud(background_color="white",width=1200, height=1000,max_words=200,
-                 min_font_size=20,
+	for i,proto in enumerate(pobj):
+		pn = pnames[i]
+		for nsauce in seenIP[pn].keys():
+			k = seenIP[pn][nsauce].keys()
+			kl = len(k)
+			print('###for',nsauce,'got',kl)
+			if kl > 1:
+				sf,newsaucen = lookup(seenIP[pn][nsauce],nsauce,deens) # expensive operation so moved here
+				if doGraphs:
+					nody = list(sf.keys())
+					nody.append(newsaucen)
+					gIP.add_nodes_from(nody)
+					edgy = [(newsaucen,x,{'weight':sf[x]}) for x in sf]
+					gIP.add_edges_from(edgy)
+				outfn = '%s_%s_wordcloud_%s.png' % (newsaucen,pn,os.path.basename(infname))
+				wc = WordCloud(background_color="white",width=1200, height=1000,max_words=200,
+				 min_font_size=20,
 				color_func=random_color_func).generate_from_frequencies(sf)
-                f = plt.figure(figsize=(10, 10))
-                plt.imshow(wc, interpolation='bilinear')
-                plt.axis('off')
-                plt.title('%s %s destination word cloud' % (nsauce,pn))
-                plt.show()
-                f.savefig(outfn, bbox_inches='tight')
-    for sport in seenPORT.keys():
-        kl = len(seenPORT[sport].keys())
-        print('###for',sport,'got',kl)
-        sf = seenPORT[sport]
-        s,p = sport.split('_')
-        sname = deens.get(s,s)
-        snameport = '%s_port_%s' % (sname,p)
-        if kl > 5:
-            print('sport=',snameport,'sf=',sf)
-            outfn = '%s_wordcloud_%s.png' % (snameport,os.path.basename(infname))
-            wc = WordCloud(background_color="white",width=1200, height=1000,
+				f = plt.figure(figsize=(50, 50))
+				plt.imshow(wc, interpolation='bilinear')
+				plt.axis('off')
+				plt.title('%s %s destination word cloud' % (nsauce,pn))
+				# plt.show()
+				f.savefig(outfn, bbox_inches='tight')
+				plt.clf() 
+
+	for sport in seenPORT.keys():
+		k = seenPORT[sport].keys()
+		kl = len(k)
+		print('###for',sport,'got',kl)
+		sf = seenPORT[sport]
+		s,p = sport.split('_')
+		sname = deens.get(s,s)
+		snameport = '%s_port_%s' % (sname,p)
+		if kl > 5:
+			if doGraphs:
+				gPORT.add_nodes_from(k)
+				pawts = [(snameport,x,{'weight':seenPORT[sport][x]}) for x in k]
+				gPORT.add_edges_from(pawts)
+			print('sport=',snameport,'sf=',sf)
+			outfn = '%s_wordcloud_%s.png' % (snameport,os.path.basename(infname))
+			wc = WordCloud(background_color="white",width=1200, height=1000,
 				max_words=200,min_font_size=10,
 				color_func=random_color_func).generate_from_frequencies(sf)
-            f = plt.figure(figsize=(10, 10))
-            plt.imshow(wc, interpolation='bilinear')
-            plt.axis('off')
-            plt.title('%s destination port word cloud' % (snameport))
-            plt.show()
-            f.savefig(outfn, bbox_inches='tight')
-    return(deens)
+			f = plt.figure(figsize=(50, 50))
+			plt.imshow(wc, interpolation='bilinear')
+			plt.axis('off')
+			plt.title('%s destination port word cloud' % (snameport))
+			# plt.show()
+			f.savefig(outfn, bbox_inches='tight')
+			plt.clf() 
+	return(deens)
 
 
-    
+	
 if __name__=="__main__":
-    seenIP,seenPORT = readPcap(infname,{},{})
-    print('## reading done')
-    deens = processPcap(seenIP,seenPORT,{})
-    print(seenIP)
-    print(seenPORT)
-    print(deens)
+	seenIP,seenPORT,allIP,allPORT = readPcap(infname,{},{})
+	print('## reading done')
+	deens = processPcap(seenIP,seenPORT,{})
+	print(seenIP)
+	print(seenPORT)
+	print(deens)
+	if doGraphs:
+		f = plt.figure(figsize=(50, 50))
+		nx.draw(gIP, with_labels=True, font_weight='bold')
+		plt.savefig('ipnet.jpg')
+		nx.draw(gPORT, with_labels=True, font_weight='bold')
+		plt.savefig('portnet.jpg')
--- pmap/portscan.py
+++ pmap/portscan.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 # adapted from https://pythonprogramming.net/python-threaded-port-scanner/
+# Nothing like as good as
+# time sudo nmap -n -PN -sT -sU -p- 127.0.0.1
+# Not shown: 131059 closed ports
+# 51413/tcp open          unknown
+# 68/udp    open|filtered dhcpc
+# 631/udp   open|filtered ipp
+# 5353/udp  open|filtered zeroconf
+# 50127/udp open|filtered unknown
+# Nmap done: 1 IP address (1 host up) scanned in 6949.53 seconds
+# but faster at about 7 seconds for the same host
+
 import threading
 from queue import Queue
 import time
@@ -15,49 +26,50 @@ q = Queue()
 
 
 def pscan(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        con = s.connect((remoteServer,port))
-        with print_lock:
-            print('port',port,'open')
-        con.close()
-    except:
-        pass
-        
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		con = s.connect((remoteServer,port))
+		with print_lock:
+			print('port',port,'open')
+		con.close()
+	except:
+		pass
+		
 def threader():
-    while True:
-        worker = q.get()
-        pscan(worker)
-        q.task_done()
-        
+	while True:
+		worker = q.get()
+		pscan(worker)
+		q.task_done()
+		
 
 def doRun():
 
-    for x in range(NTHREADS):
-         t = threading.Thread(target=threader)
-         # classify as a daemon, so they will die when the main dies
-         t.daemon = True
-         # begins, must come after daemon definition
-         t.start()
-    for worker in range(1,LASTPORT):
-        q.put(worker)
-    # wait until the q terminates.
-    q.join()
+	for x in range(NTHREADS):
+		 t = threading.Thread(target=threader)
+		 # classify as a daemon, so they will die when the main dies
+		 t.daemon = True
+		 # begins, must come after daemon definition
+		 t.start()
+	for worker in range(1,LASTPORT):
+		q.put(worker)
+	# wait until the q terminates.
+	q.join()
 
 if __name__ == "__main__":
-    # Ask for input
-    remoteServer    = input("Enter a remote host to scan: ")
-    remoteServerIP  = socket.gethostbyname(remoteServer)
+	# Ask for input
+	remoteServer    = input("Enter a remote host to scan (127.0.0.1 is default): ")
+	if not '.' in remoteServer: 
+		remoteServer = '127.0.0.1'
+	remoteServerIP  = socket.gethostbyname(remoteServer)
 
-    # Print a nice banner with information on which host we are about to scan
-    print("-" * 60)
-    print("Please wait, scanning remote host", remoteServerIP)
-    print("-" * 60)
+	# Print a nice banner with information on which host we are about to scan
+	foo = "-" * 60 + '\n'
+	print(foo+"Please wait, scanning remote host %s \n" % remoteServerIP+foo)
 
 
-    t1 = datetime.now()
-    doRun()
+	t1 = datetime.now()
+	doRun()
 
-    t2 = datetime.now()
-    total =  t2 - t1
-    print ('Scanning Completed in: ', total,'secs')
+	t2 = datetime.now()
+	total =  t2 - t1
+	print ('Scanning Completed in: ', total,'secs')
